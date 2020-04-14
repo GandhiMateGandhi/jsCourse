d568deed6e75e1072c7b5734202540883d23593d
Baibulat <surersculptor@gmail.com>
2020-04-15 00:13:59 +0500

    just practice

--- Practice/index.js
+++ Practice/index.js
@@ -1,6 +1,26 @@
+let num = 100;
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+/* if (num < 90) {
+    console.log('Nope');
+} else if (num < 11) {
+    console.log('Yep');
+} else {
+    console.log('Nothing is right');
+}
 
-newArr = arr.concat('lol', 'lul');
-console.log(arr);
-console.log(newArr);
+(num == 100) ? console.log('Yep') : console.log('Nope'); */
+
+switch (num) {
+    case num > 90:
+        console.log('more than 90');
+        break;
+    case num <= 100:
+        console.log('less than 110');
+        break;
+    case 100:
+        console.log('Thats right');
+        break;
+    default:
+        console.log('idk whats going on');
+        break;
+}
